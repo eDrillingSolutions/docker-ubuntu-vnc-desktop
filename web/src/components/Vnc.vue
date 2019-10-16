3d383071dacfb856ba336ffdea668f442229c6ec
Stig Helgeland <sh@edrilling.no>
2019-10-16 12:30:00 +0200

    Change the name of the browser window

--- web/src/components/Vnc.vue
+++ web/src/components/Vnc.vue
@@ -112,7 +112,7 @@ export default {
         let url = 'static/vnc.html?'
         url += 'autoconnect=1&'
         url += `host=${hostname}&port=${port}&`
-        url += `path=${websockifyPath}&title=novnc2&`
+        url += `path=${websockifyPath}&title=wellViz3D&`
         url += `logging=warn&`
         url += 'resize=scale&'
         url += 'reconnect=true&'
